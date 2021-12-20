e98ac66cf648c08d0c1c774f15330255594f1c8b
DellsadaLvt <levantho0168@gmail.com>
2021-12-20 15:03:37 +0700

    lib GLCD

---
+++ MDK-ARM/GLCD_SPI/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+glcd_spi\startup_stm32f103xb.o: startup_stm32f103xb.s
---
+++ MDK-ARM/User/Include/user_err.h
@@ -0,0 +1,27 @@
+#ifndef _USER_ERR_H_
+
+#define _USER_ERR_H_
+
+typedef enum{
+	oke,
+	fail
+} user_func_status_t;
+
+
+
+
+#endif   /* _USER_ERR_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
